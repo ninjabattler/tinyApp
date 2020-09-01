7fa0932faf12b564f1302ba9cc2edfe2874994ba
ninjabattler <jaydenrtucker@gmail.com>
2020-09-01 20:44:19 +0000

    Added delete buttons that send POST requests to the server and delete the selected url from the database

--- express_server.js
+++ express_server.js
@@ -1,32 +1,62 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//All shortend urls and the full url they reference
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+function generateRandomString() {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+  let randomString = "";
+
+  for(let i = 1; i <= 6; i++){
+
+    randomString += letters[Math.floor(Math.random() * letters.length)];
+
+  }
+
+  return randomString;
+}
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urlsIndex", templateVars);
 });
+//Takes a post request for /urls and generates a new shortened url
+app.post("/urls", (req, res) => {
+  let rand = generateRandomString();
+  urlDatabase[rand] = req.body.longURL;
+  let templateVars = { shortURL: rand, longURL: req.body.longURL };
+  res.render("urlsShow", templateVars);    
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urlsNew");
 });
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  let templateVars = { urls: urlDatabase };
+  res.redirect("/urls");
+});
+
+//Redirect user to the longUrl attached to the entered shorUrl
+app.get("/u/:shortURL", (req, res) => {
+  res.redirect(urlDatabase[req.params.shortURL]);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urlsShow", templateVars);
 });
 
+//Start Listening
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
